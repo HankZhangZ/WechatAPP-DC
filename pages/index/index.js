7ced031702b7bb0a2064ed386a1d799f23ec523c
HankZ <781574233@qq.com>
2020-06-10 11:12:59 +0800

    123

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '邪王真眼是最强的!',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
