570a053ccd99152a1fd44207827223d0d71b8981
djddskkekk <66710149+djddskkekk@users.noreply.github.com>
2020-06-10 14:14:34 +0800

    1.0.3

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,34 +1,9 @@
 //app.js
 App({
   onLaunch: function () {
-    console.log ( "app.js初始化" )
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift ( Date.now() )
-    wx.setStorageSync ( 'logs', logs )
-    wx.login({
-      success: res => {
-        console.log ( 'we.login登录成功' )
-      }
-    })
-    wx.getSetting({
-      success: res => {
-        console.log ( '获取用户的当前设置，返回授权后的信息' )
-        console.log(res)
-        if ( res.authSetting[ 'scope.userInfo' ] ) {
-          console.log ( "用户已经授权" )
-          wx.getUserInfo({
-            success: res => {
-              this.globalData.userInfo = res.userInfo
-              console.log ( 'app.js获取用户信息成功' )
-              console.log ( this.globalData.userInfo )
-              if ( this.userInfoReadyCallback ) {
-                this.userInfoReadyCallback ( res )
-              }
-            }
-          })
-        }
-      }
-    })
+   
   },
-  
+  globalData: {
+    
+  }
 })
